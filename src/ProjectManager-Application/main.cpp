0423044aad120e90b3e2349d5ff8a42e8e722ef3
GustavJones <gustavjones3@gmail.com>
2024-08-25 21:36:41 +0200

    Add more functionality
    Add the create, delete and list_templates commands

--- src/ProjectManager-Application/main.cpp
+++ src/ProjectManager-Application/main.cpp
@@ -8,12 +8,14 @@ int main(int argc, char *argv[]) {
 
   app.parser.AddStructure(
       "[flags:value_amount=0,argument_filter=--,help=Flag "
-      "Values;command:help=Run Command;template_name:help=The "
+      "Values;command:help=Run Command;name:help=The "
       "Name of the Project to apply command;directory:help=Path to directory]");
 
   app.parser.AddKey(GArgs::Key("flags", "--help", "Display's this message"));
   app.parser.AddKey(
       GArgs::Key("flags", "--template_dir", "Sets template directory"));
+  app.parser.AddKey(
+      GArgs::Key("command", "list_templates", "List available templates"));
   app.parser.AddKey(GArgs::Key("command", "create", "Creates a new Project"));
   app.parser.AddKey(
       GArgs::Key("command", "delete", "Deletes an existing Project"));
@@ -21,7 +23,7 @@ int main(int argc, char *argv[]) {
                                "Adds a new Project template to generate"));
   app.parser.AddKey(GArgs::Key("command", "remove_template",
                                "Removes an existing Project template"));
-  app.parser.AddKey(GArgs::Key("template_name", "*", "Template Name"));
+  app.parser.AddKey(GArgs::Key("name", "*", "Template Name"));
   app.parser.AddKey(GArgs::Key("directory", "*", "Directory path"));
 
   app.parser.ParseArgs(argc, argv);
@@ -46,12 +48,12 @@ int main(int argc, char *argv[]) {
 
   // Remove template
   if (app.parser.Contains("command", "remove_template")) {
-    if (app.parser["template_name"] == "") {
+    if (app.parser["name"] == "") {
       std::cerr << "Template name not given" << std::endl;
       return 1;
     } else {
       const std::string templatePath =
-          app.templateDir + "/" + app.parser["template_name"];
+          app.templateDir + "/" + app.parser["name"];
 
       if (!app.Exists(templatePath)) {
         std::cout << "Template doesn't exist" << std::endl;
@@ -75,12 +77,12 @@ int main(int argc, char *argv[]) {
       return 1;
     }
 
-    if (app.parser["template_name"] == "") {
+    if (app.parser["name"] == "") {
       std::cerr << "Template name not given" << std::endl;
       return 1;
     } else {
       const std::string templatePath =
-          app.templateDir + '/' + app.parser["template_name"];
+          app.templateDir + '/' + app.parser["name"];
 
       const std::string copyPath = app.parser["directory"];
 
@@ -95,6 +97,66 @@ int main(int argc, char *argv[]) {
           std::cout << "Failed to create template" << std::endl;
           return 1;
         }
+      }
+    }
+  }
+
+  // List Templates
+  if (app.parser.Contains("command", "list_templates")) {
+    std::cout << "Template projects: " << std::endl;
+
+    for (const auto &dir : app.ListDir(app.templateDir)) {
+      std::cout << dir << std::endl;
+    }
+
+    return 0;
+  }
+
+  // Delete Project
+  if (app.parser.Contains("command", "delete")) {
+    if (app.parser["directory"] == "") {
+      std::cerr << "No directory given for source" << std::endl;
+      return 1;
+    }
+
+    if (app.parser["name"] == "") {
+      std::cerr << "Project name not given" << std::endl;
+      return 1;
+    } else {
+      const std::string projectPath =
+          app.parser["directory"] + '/' + app.parser["name"];
+
+      if (app.RemoveDir(projectPath)) {
+        std::cout << "Deleted project " << app.parser["name"] << std::endl;
+        return 0;
+      } else {
+        std::cerr << "Failed to delete project" << std::endl;
+        return 1;
+      }
+    }
+  }
+
+  // Create Project
+  if (app.parser.Contains("command", "create")) {
+    if (app.parser["directory"] == "") {
+      std::cerr << "No directory given for source" << std::endl;
+      return 1;
+    }
+
+    if (app.parser["name"] == "") {
+      std::cerr << "Project template not given" << std::endl;
+      return 1;
+    } else {
+      const std::string projectPath =
+          app.parser["directory"] + '/' + app.parser["name"];
+
+      if (app.CopyDir(projectPath,
+                      app.templateDir + '/' + app.parser["name"])) {
+        std::cout << "Project created from " << app.parser["name"] << std::endl;
+        return 0;
+      } else {
+        std::cerr << "Failed to create project" << std::endl;
+        return 1;
       }
     }
   }
