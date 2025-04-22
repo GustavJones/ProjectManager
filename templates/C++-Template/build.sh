1c99b24979ff1adcb227be83ff8e252e79e72804
GustavJones <gustavjones3@gmail.com>
2025-04-22 22:07:17 +0200

    Add better support for Windows

--- templates/C++-Template/build.sh
+++ templates/C++-Template/build.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)
 
 # Configure project
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
 
 # Build project
 cmake --build build -j
