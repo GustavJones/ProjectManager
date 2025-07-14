6232d3894bebe192c79f41c3f17b2c8f709061d8
GustavJones <gustavjones3@gmail.com>
2025-07-14 05:54:41 +0200

    Add a control variable for build batch scripts

--- templates/ASM-Template/build.bat
+++ templates/ASM-Template/build.bat
@@ -1,10 +1,13 @@
 @echo off
 
+:: CMake Build Type
+set BUILD_TYPE=Debug
+
 :: Set current directory to script directory
 cd /d "%~dp0"
 
 :: Configure cmake project
-cmake.exe -S . -B build -DCMAKE_BUILD_TYPE=Debug
+cmake.exe -S . -B build -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
 
 :: Build cmake project
-cmake --build build --config Debug -j
+cmake --build build --config %BUILD_TYPE% -j
